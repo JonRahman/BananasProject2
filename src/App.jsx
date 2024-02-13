3805ea7e64c78e239d4b284019d957631f6c7dab
JonRahman <jonrahman90@gmail.com>
2024-02-13 21:12:56 +0000

    updated files and removed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Layout from "./Layout";
 import Navbar from './Components/Home/Navbar';
 import Footer from './Components/Footer';
+import Home from "./Pages/Home";
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
       <div className="App">
         <Navbar />
-           <Layout />
+        <Home />
+            {<BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </BrowserRouter>}
         <Footer />
       </div>
     );
